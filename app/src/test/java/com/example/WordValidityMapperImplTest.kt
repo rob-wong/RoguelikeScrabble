b763a766cab39ee8e5db6f5a72bbadcfd214a6e3
Rob <business@robsw.ca>
2025-09-27 15:40:44 -0400

    lint

--- app/src/test/java/com/example/WordValidityMapperImplTest.kt
+++ app/src/test/java/com/example/WordValidityMapperImplTest.kt
@@ -9,18 +9,18 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class WordValidityMapperImplTest {
-	
+
 	private val sut = WordValidityMapperImpl(validWords)
-	
+
 	@Test
-	fun `Given valid word, When map, Then true expected`() = runTest { 
+	fun `Given valid word, When map, Then true expected`() = runTest {
 		// Given
 		val letters = listOf<Letter>(
-			DefaultLetter("", 'v',0),
-			DefaultLetter("", 'a',0),
-			DefaultLetter("", 'l',0),
-			DefaultLetter("", 'i',0),
-			DefaultLetter("", 'd',0),
+			DefaultLetter("", 'v', 0),
+			DefaultLetter("", 'a', 0),
+			DefaultLetter("", 'l', 0),
+			DefaultLetter("", 'i', 0),
+			DefaultLetter("", 'd', 0),
 		)
 
 		// When
@@ -38,14 +38,14 @@ class WordValidityMapperImplTest {
 	fun `Given invalid word containing valid word, When map, Then false expected`() = runTest {
 		// Given
 		val letters = listOf<Letter>(
-			DefaultLetter("", 'v',0),
-			DefaultLetter("", 'a',0),
-			DefaultLetter("", 'l',0),
-			DefaultLetter("", 'i',0),
-			DefaultLetter("", 'd',0),
-			DefaultLetter("", 'i',0),
-			DefaultLetter("", 't',0),
-			DefaultLetter("", 'y',0),
+			DefaultLetter("", 'v', 0),
+			DefaultLetter("", 'a', 0),
+			DefaultLetter("", 'l', 0),
+			DefaultLetter("", 'i', 0),
+			DefaultLetter("", 'd', 0),
+			DefaultLetter("", 'i', 0),
+			DefaultLetter("", 't', 0),
+			DefaultLetter("", 'y', 0),
 		)
 
 		// When
@@ -63,7 +63,7 @@ class WordValidityMapperImplTest {
 	fun `Given1 letter word, When map, Then false expected`() = runTest {
 		// Given
 		val letters = listOf<Letter>(
-			DefaultLetter("", 'i',0)
+			DefaultLetter("", 'i', 0)
 		)
 
 		// When
