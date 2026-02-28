774976952c6fcb790c22fb848e3783fa4d753dab
Rob <business@robsw.ca>
2026-02-28 10:38:38 -0500

    Device Previews update

--- app/src/main/kotlin/com/cypherose/app/util/DevicePreviews.kt
+++ app/src/main/kotlin/com/cypherose/app/util/DevicePreviews.kt
@@ -2,7 +2,13 @@ package com.cypherose.app.util
 
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview(name = "Phone Portrait", widthDp = 400, heightDp = 800)
-@Preview(name = "Tablet Square", widthDp = 800, heightDp = 800)
-@Preview(name = "Tablet Landscape", widthDp = 800, heightDp = 400)
+// Phone sizes
+@Preview(name = "Phone Portrait", widthDp = 411, heightDp = 731)
+@Preview(name = "Phone Landscape", widthDp = 731, heightDp = 411)
+
+// Tablet sizes
+@Preview(name = "Small Tablet Portrait", widthDp = 600, heightDp = 960)
+@Preview(name = "Small Tablet Landscape", widthDp = 960, heightDp = 600)
+@Preview(name = "Large Tablet Portrait", widthDp = 1024, heightDp = 1366)
+@Preview(name = "Large Tablet Landscape", widthDp = 1366, heightDp = 1024)
 annotation class DevicePreviews
